7cd61b074b4d44507330465057b178d155e93761
E132 <107452563+132E@users.noreply.github.com>
2022-07-05 23:50:26 -0500

    CSS and HTML  ready, now the hard part

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const API_KEY = "1";
-const DOMAIN = "https://www.themealdb.com/api.php";
-const IMAGE_BASE_PATH =
-  "https://images/media/meals/llcbn01574260722.jpg/preview";
-const MEALS_BY_NAME =
-  "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search_result");
+const container = document.querySelector(".container");
+let searchQuery = "";
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+  console.log("searchQuery");
+});
