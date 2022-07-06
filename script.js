069b582e5f0ffe25cf7c9b6258b97d01fb337974
E132 <107452563+132E@users.noreply.github.com>
2022-07-05 20:19:33 -0500

    HTML set up

---
+++ script.js
@@ -0,0 +1,6 @@
+const API_KEY = "1";
+const DOMAIN = "https://www.themealdb.com/api.php";
+const IMAGE_BASE_PATH =
+  "https://images/media/meals/llcbn01574260722.jpg/preview";
+const MEALS_BY_NAME =
+  "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
