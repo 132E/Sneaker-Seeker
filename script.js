3b143d3c6937c1eb578820166c1705c5dd6e8ac8
E132 <107452563+132E@users.noreply.github.com>
2022-07-06 10:29:44 -0500

    working on API call

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search_result");
 const container = document.querySelector(".container");
 
+const API_ID = "1";
+const API_key = "1";
+
 let searchQuery = "";
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
-  console.log(searchQuery);
+  fetchAPI();
 });
+
+async function fetchAPI() {
+  const baseURL = `www.themealdb.com/api/json/v1/1/search.php?s=Chicken&api_id=${API_ID}&api_key=${API_key}`;
+  const response = await fetch(baseURL);
+  console.log(response);
+}
