380fb80822588d2022cc977e8dce5e15da5cf334
E132 <107452563+132E@users.noreply.github.com>
2022-07-07 11:13:32 -0500

    reworking js file

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search_result");
 const container = document.querySelector(".container");
+const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`;
 
-const API_ID = "1";
-const API_key = "1";
-
-let searchQuery = "";
-
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  searchQuery = e.target.querySelector("input").value;
-  fetchAPI();
-});
-
-async function fetchAPI() {
-  const baseURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=chicken&api_id=${API_ID}&api_key=${API_key}`;
-  const response = await fetch(baseURL);
+searchForm.addEventListener("click", async () => {
+  let response = await axios.get(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  );
   const data = await response.json();
   console.log(data);
-}
+  searchResultDiv.innerHTML = `<img src="${data.avatar_url}" alt="${data.login}">`;
+  container.style.display = "block";
+});
